45552c5a911ff68c9b507dfe8540c14a1a70ccf9
Thomas <thomas.rundle@conduitlearning.com>
2016-11-04 13:36:25 -0500

    Copy seed for table exercise

---
+++ table/app/project-center/project-center.module.js
@@ -0,0 +1,29 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var shared_module_1 = require('../shared/shared.module');
+var project_center_component_1 = require('./project-center.component');
+var ProjectCenterModule = (function () {
+    function ProjectCenterModule() {
+    }
+    ProjectCenterModule = __decorate([
+        core_1.NgModule({
+            imports: [shared_module_1.SharedModule],
+            declarations: [project_center_component_1.ProjectCenterComponent],
+            providers: [],
+            exports: [project_center_component_1.ProjectCenterComponent]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ProjectCenterModule);
+    return ProjectCenterModule;
+}());
+exports.ProjectCenterModule = ProjectCenterModule;
+//# sourceMappingURL=project-center.module.js.map
