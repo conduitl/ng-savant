f5bcc2c501b8a33fb192febaa030b0959585aee6
Thomas <thomas.rundle@conduitlearning.com>
2016-12-05 15:42:57 -0600

    Webpack demo initial setup

---
+++ webpack/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+    entry: "./main",
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    watch: true,
+    devServer: {
+        contentBase: '.'
+    }
+};
